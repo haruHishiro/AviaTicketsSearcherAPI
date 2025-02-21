a50b2dc6c4faa4f464ae58509be538681e17b357
Konstantin Panov <104938827+haruHishiro@users.noreply.github.com>
2025-02-21 23:11:06 +0300

    add mapping for link

--- src/main/java/com/avia/tickets/searching/controllers/UsersController.java
+++ src/main/java/com/avia/tickets/searching/controllers/UsersController.java
@@ -10,6 +10,7 @@ package com.avia.tickets.searching.controllers;
 import com.avia.tickets.searching.models.ValueLong;
 import com.avia.tickets.searching.response.Response;
 import com.avia.tickets.searching.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UsersController {
     UserService userService;
+
+    @GetMapping("/getInternalUserIdViaTelegramId")
     public Response getInternalUserIdViaTelegramId(@RequestParam long telegramId) {
         Response response = Response.builder()
                 .setCode(200)
