9bb029954726bf9a30c9c5d2d5e7ba570181d480
Konstantin Panov <104938827+haruHishiro@users.noreply.github.com>
2025-02-21 23:00:25 +0300

    add class for processing responses

---
+++ src/main/java/com/avia/tickets/searching/response/Response.java
@@ -0,0 +1,52 @@
+/*
+ * Class for processing responses
+ * Developers: Panov K.D. ;
+ * Last change: february 2025
+ */
+
+package com.avia.tickets.searching.response;
+
+
+import com.avia.tickets.searching.models.BaseModel;
+
+
+public class Response {
+    private int code;
+    private String status;
+    private String description;
+    private BaseModel body;
+
+    public static ResponseBuilder builder() {
+        return new Response().new ResponseBuilder();
+    }
+
+    public class ResponseBuilder {
+        private ResponseBuilder() {
+        }
+
+        public ResponseBuilder setCode(int code) {
+            Response.this.code = code;
+            return this;
+        }
+
+        public ResponseBuilder setStatus(String status) {
+            Response.this.status = status;
+            return this;
+        }
+
+        public ResponseBuilder setDescription(String description) {
+            Response.this.description = description;
+            return this;
+        }
+
+        public ResponseBuilder setResponseBody(BaseModel model) {
+            Response.this.body = model;
+            return this;
+        }
+
+        public Response build() {
+            return Response.this;
+        }
+    }
+
+}
