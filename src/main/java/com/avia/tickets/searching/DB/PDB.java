8140e685d5b2f1029a5dbefa9afe30b3d99debd9
Konstantin Panov <104938827+haruHishiro@users.noreply.github.com>
2025-02-21 23:00:08 +0300

    add class for processing work withs database

---
+++ src/main/java/com/avia/tickets/searching/DB/PDB.java
@@ -0,0 +1,14 @@
+/*
+ * Class for work with PostgreSQL database
+ * Developers: Panov K.D. ;
+ * Last change: february 2025
+ */
+
+package com.avia.tickets.searching.DB;
+
+public class PDB {
+    public long getUserIdViaTelegramId(long telegramId) {
+        //TODO the method should access the database and get the internal user ID via the telegram identifier
+        return 1;
+    }
+}
