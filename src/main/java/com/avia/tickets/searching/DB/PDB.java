8e26b639411230dc3082a56e38e9d0bd06187289
MihOn4ik20 <evgl20102005@gmail.com>
2025-02-23 23:03:35 +0300

    Added DB connection

--- src/main/java/com/avia/tickets/searching/DB/PDB.java
+++ src/main/java/com/avia/tickets/searching/DB/PDB.java
@@ -10,7 +10,9 @@ import com.avia.tickets.searching.models.Offer;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.DriverManager;
+//import java.util.Date;
 
 public class PDB {
     /*
@@ -192,4 +194,41 @@ public class PDB {
      *                               Offer service section END
      * =====================================================================================
      */
+
+    //  Database credentials
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/AirBase";
+    static final String USER = "postgres";
+    static final String PASS = "toor";
+
+    public static void main(String[] argv) {
+
+        System.out.println("Testing connection to PostgreSQL JDBC");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("PostgreSQL JDBC Driver successfully connected");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection(DB_URL, USER, PASS);
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You successfully connected to database now");
+        } else {
+            System.out.println("Failed to make connection to database");
+        }
+    }
 }
