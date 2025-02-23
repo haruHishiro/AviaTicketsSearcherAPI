5d0ff4b3ac36bd74641ed0067833857e4df5262c
Konstantin Panov <104938827+haruHishiro@users.noreply.github.com>
2025-02-23 15:46:54 +0300

    finalize model

--- src/main/java/com/avia/tickets/searching/models/ValueLongModel.java
+++ src/main/java/com/avia/tickets/searching/models/ValueLongModel.java
@@ -7,7 +7,7 @@
 
 package com.avia.tickets.searching.models;
 
-public class ValueLongModel extends BaseModel {
+public final class ValueLongModel extends BaseModel {
     private long value;
 
     public ValueLongModel(long value) {
