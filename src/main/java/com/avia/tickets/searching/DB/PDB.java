49d142424f0bd1614d75dd6e298489e6e048c97d
Konstantin Panov <104938827+haruHishiro@users.noreply.github.com>
2025-02-23 18:40:21 +0300

    add functions prototypes to interact with the database for OffersController

--- src/main/java/com/avia/tickets/searching/DB/PDB.java
+++ src/main/java/com/avia/tickets/searching/DB/PDB.java
@@ -6,6 +6,11 @@
 
 package com.avia.tickets.searching.DB;
 
+import com.avia.tickets.searching.models.Offer;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class PDB {
     /*
      * =====================================================================================
@@ -25,6 +30,32 @@ public class PDB {
      * | isActive | <-- If user allowed / disallowed messaging with bot || True - allowed; False - disallowed
      * +==========+
      *
+     * +==========+
+     * |  offers  | <-- Table Name
+     * +==========+
+     * |    id    | <-- Offer identifier
+     * +----------+
+     * |  user id | <-- Owner id (user internal id)
+     * +----------+
+     * | isActive | <-- If offer allowed / disallowed to parse / scrap || True - allowed; False - disallowed
+     * +----------+
+     * | dep type | <-- Departure point type id
+     * +----------+
+     * | dst type | <-- Destination point type id
+     * +----------+
+     * |  dep id  | <-- Departure point internal id
+     * +----------+
+     * |  dst id  | <-- Destination point internal id
+     * +----------+
+     * |  st date | <-- Date of dispatch
+     * +----------+
+     * | end date | <-- Return ticket date
+     * +----------+
+     * |  luggage | <-- is luggage need || True - need; False - not need
+     * +----------+
+     * | max cost | <-- ticket max cost
+     * +==========+
+     *
      */
 
     /*
@@ -35,7 +66,7 @@ public class PDB {
 
     /*
      * =====================================================================================
-     *                               user service section START
+     *                               User service section START
      * =====================================================================================
      */
     public boolean isActiveUserViaTelegramId(long telegramId) {
@@ -95,7 +126,64 @@ public class PDB {
 
     /*
      * =====================================================================================
-     *                               user service section END
+     *                               User service section END
+     * =====================================================================================
+     */
+
+    /*
+     * =====================================================================================
+     *                               Offer service section START
+     * =====================================================================================
+     */
+
+    public ArrayList<Offer> getUserOffersViaTelegramId(long telegramId) {
+        //TODO the method should return list of all offers of user with the passed telegram identifier
+        return new ArrayList<Offer>();
+    }
+
+    public ArrayList<Offer> getUserOffersViaInternalId(long internalId) {
+        //TODO the method should return list of all offers of user with the passed internal identifier
+        return new ArrayList<Offer>();
+    }
+
+    public ArrayList<Offer> getUserActiveOffersViaTelegramId(long telegramId) {
+        //TODO the method should return list of active offers of user with the passed telegram identifier
+        return new ArrayList<Offer>();
+    }
+
+    public ArrayList<Offer> getUserActiveOffersViaInternalId(long internalId) {
+        //TODO the method should return list of active offers of user with the passed internal identifier
+        return new ArrayList<Offer>();
+    }
+
+    public ArrayList<Offer> getUserNonActiveOffersViaTelegramId(long telegramId) {
+        //TODO the method should return list of non active offers of user with the passed telegram identifier
+        return new ArrayList<Offer>();
+    }
+
+    public ArrayList<Offer> getUserNonActiveOffersViaInternalId(long internalId) {
+        //TODO the method should return list of non active offers of user with the passed internal identifier
+        return new ArrayList<Offer>();
+    }
+
+    public boolean addOffer(long internalId, Offer offerFields) {
+        //TODO the method should create a new offer for user with passed internal identifier if offer with the passed fields for this user not exist
+        return false;
+    }
+
+    public boolean disableOffer(long offerId) {
+        //TODO the method should set field isActive to false of offer with the passed identifier
+        return false;
+    }
+
+    public boolean enableOffer(long offerId) {
+        //TODO the method should set field isActive to true of offer with the passed identifier
+        return false;
+    }
+
+    /*
+     * =====================================================================================
+     *                               Offer service section END
      * =====================================================================================
      */
 }
