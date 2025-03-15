59fbd409f0a262c6d5ab6df1367b28ca3ecdefea
Konstantin Panov <104938827+haruHishiro@users.noreply.github.com>
2025-03-15 18:24:42 +0300

    add String model

---
+++ src/main/java/com/avia/tickets/searching/models/StringModel.java
@@ -0,0 +1,18 @@
+package com.avia.tickets.searching.models;
+
+public class StringModel extends BaseModel{
+    String value;
+
+    public StringModel(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
+
