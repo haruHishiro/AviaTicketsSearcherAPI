f7ce102a688d70bdb0969b1bbd300e9bf36c1258
Konstantin Panov <104938827+haruHishiro@users.noreply.github.com>
2025-03-15 18:25:15 +0300

    add token processing

--- src/main/java/com/avia/tickets/searching/controllers/ParserController.java
+++ src/main/java/com/avia/tickets/searching/controllers/ParserController.java
@@ -7,11 +7,50 @@
 package com.avia.tickets.searching.controllers;
 
 
+import com.avia.tickets.searching.models.StringModel;
+import com.avia.tickets.searching.models.ValueBooleanModel;
+import com.avia.tickets.searching.response.Response;
+import com.avia.tickets.searching.services.ParserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api/v1/parser")
 public class ParserController {
+    private final ParserService parserService;
+
+    @Autowired
+    public ParserController(ParserService parserService) {
+        this.parserService = parserService;
+    }
+
+    @GetMapping("/getToken")
+    public Response getToken() {
+
+        Response response = new Response();
+        response = Response.builder()
+                .setCode(200)
+                .setStatus("OK")
+                .setDescription("method in develop")
+                .setResponseBody(new StringModel(parserService.getToken()))
+                .build();
+        return response;
+    }
+
+    @GetMapping("/setToken")
+    public Response setToken(@RequestParam String token) {
+        parserService.setToken(token);
+        Response response = new Response();
+        response = Response.builder()
+                .setCode(200)
+                .setStatus("OK")
+                .setDescription("method in develop")
+                .setResponseBody(new StringModel("success"))
+                .build();
+        return response;
+    }
 }
--- src/main/java/com/avia/tickets/searching/services/ParserService.java
+++ src/main/java/com/avia/tickets/searching/services/ParserService.java
@@ -12,4 +12,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ParserService {
+    private String token = "";
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
