617159b94dedd8ce39962ea494a4dbadc818fb37
Konstantin Panov <104938827+haruHishiro@users.noreply.github.com>
2025-04-05 16:10:59 +0300

    add logs and responses

--- src/main/java/com/avia/tickets/searching/controllers/UsersController.java
+++ src/main/java/com/avia/tickets/searching/controllers/UsersController.java
@@ -32,18 +32,23 @@ public class UsersController {
 
     @GetMapping("/isActiveUser")
     public Response isActiveUser(@RequestParam long userId, @RequestParam boolean isInternalId) {
-        //TODO logs to console
-        Response response = new Response();
-
+        Response response;
         try {
             response = Response.builder()
                     .setCode(200)
                     .setStatus("OK")
-                    .setDescription("method in develop")
+                    .setDescription("success")
                     .setResponseBody(new ValueBooleanModel(userService.isActiveUser(userId, isInternalId)))
                     .build();
+            System.out.println("[LOG] [isActiveUser] [SUCCESS]");
         } catch (SQLException e) {
             System.out.println(e);
+            response = Response.builder()
+                    .setCode(200)
+                    .setStatus("OK")
+                    .setDescription("error")
+                    .build();
+            System.out.println("[LOG] [isActiveUser] [ERROR] [userId] " + userId + " [isInternalId] " + isInternalId);
         }
 
         return response;
@@ -51,110 +56,138 @@ public class UsersController {
 
     @GetMapping("/disableUser")
     public Response disableUser(@RequestParam long userId, @RequestParam boolean isInternalId) {
-        //TODO logs to console
-        Response response = new Response();
-
+        Response response;
         try {
+            userService.disableUser(userId, isInternalId);
             response = Response.builder()
                     .setCode(200)
                     .setStatus("OK")
-                    .setDescription("method in develop")
-                    .setResponseBody(new ValueBooleanModel(userService.disableUser(userId, isInternalId)))
+                    .setDescription("success")
                     .build();
+            System.out.println("[LOG] [disableUser] [SUCCESS]");
         } catch (SQLException e) {
             System.out.println(e);
+            response = Response.builder()
+                    .setCode(200)
+                    .setStatus("OK")
+                    .setDescription("error")
+                    .build();
+            System.out.println("[LOG] [disableUser] [ERROR] [userId] " + userId + " [isInternalId] " + isInternalId);
         }
-
-
         return response;
     }
 
     @GetMapping("/enableUser")
     public Response enableUser(@RequestParam long userId, @RequestParam boolean isInternalId) {
-        //TODO logs to console
-        Response response = new Response();
-
+        Response response;
         try {
+            userService.enableUser(userId, isInternalId);
             response = Response.builder()
                     .setCode(200)
                     .setStatus("OK")
-                    .setDescription("method in develop")
-                    .setResponseBody(new ValueBooleanModel(userService.enableUser(userId, isInternalId)))
+                    .setDescription("success")
                     .build();
+            System.out.println("[LOG] [enableUser] [SUCCESS]");
         } catch (SQLException e) {
             System.out.println(e);
+            response = Response.builder()
+                    .setCode(200)
+                    .setStatus("OK")
+                    .setDescription("success")
+                    .build();
+            System.out.println("[LOG] [enableUser] [ERROR] [userId] " + userId + " [isInternalId] " + isInternalId);
         }
-
         return response;
     }
 
     @GetMapping("/createUser")
     public Response createUser(@RequestParam long telegramId) {
-        //TODO logs to console
-        Response response = new Response();
+        Response response;
         try {
             userService.createUser(telegramId);
             response = Response.builder()
                     .setCode(200)
                     .setStatus("OK")
-                    .setDescription("method in develop")
-                    .setResponseBody(new ValueBooleanModel(true))
+                    .setDescription("success")
                     .build();
+            System.out.println("[LOG] [createUser] [SUCCESS]");
         } catch (SQLException e) {
             System.out.println(e);
+            response = Response.builder()
+                    .setCode(200)
+                    .setStatus("OK")
+                    .setDescription("error")
+                    .build();
+            System.out.println("[LOG] [createUser] [ERROR] [telegramId] " + telegramId);
         }
         return response;
     }
 
     @GetMapping("/isUserExist")
     public Response isUserExist(@RequestParam long userId, @RequestParam boolean isInternalId) {
-        //TODO logs to console
-        Response response = new Response();
-
+        Response response;
         try {
             response = Response.builder()
                     .setCode(200)
                     .setStatus("OK")
-                    .setDescription("method in develop")
+                    .setDescription("success")
                     .setResponseBody(new ValueBooleanModel(userService.isUserExist(userId, isInternalId)))
                     .build();
+            System.out.println("[LOG] [isUserExist] [SUCCESS]");
         } catch (SQLException e) {
             System.out.println(e);
+            response = Response.builder()
+                    .setCode(200)
+                    .setStatus("OK")
+                    .setDescription("error")
+                    .build();
+            System.out.println("[LOG] [isUserExist] [ERROR] [userId] " + userId + " [isInternalId] " + isInternalId);
         }
-
         return response;
     }
 
     @GetMapping("/getInternalUserIdViaTelegramId")
     public Response getInternalUserIdViaTelegramId(@RequestParam long telegramId) {
-        //TODO logs to console
         Response response = new Response();
         try {
             response = Response.builder()
                     .setCode(200)
                     .setStatus("OK")
-                    .setDescription("method in develop")
+                    .setDescription("success")
                     .setResponseBody(new ValueLongModel(userService.getInternalIdViaTelegramId(telegramId)))
                     .build();
+            System.out.println("[LOG] [getInternalUserIdViaTelegramId] [SUCCESS]");
         } catch (SQLException e) {
             System.out.println(e);
+            response = Response.builder()
+                    .setCode(200)
+                    .setStatus("OK")
+                    .setDescription("error")
+                    .build();
+            System.out.println("[LOG] [getInternalUserIdViaTelegramId] [ERROR] [telegramId] " + telegramId);
         }
         return response;
     }
 
     @GetMapping("/getTelegramIdViaInternalUserId")
     public Response getTelegramIdViaInternalUserId(@RequestParam long internalId) {
-        //TODO logs to console
         Response response = new Response();
         try {
             response = Response.builder()
                     .setCode(200)
                     .setStatus("OK")
-                    .setDescription("method in develop")
+                    .setDescription("success")
                     .setResponseBody(new ValueLongModel(userService.getTelegramIdViaInternalId(internalId)))
                     .build();
+            System.out.println("[LOG] [getTelegramIdViaInternalUserId] [SUCCESS]");
         } catch (SQLException e) {
             System.out.println(e);
+            response = Response.builder()
+                    .setCode(200)
+                    .setStatus("OK")
+                    .setDescription("error")
+                    .build();
+            System.out.println("[LOG] [getTelegramIdViaInternalUserId] [ERROR] [internalId] " + internalId);
         }
         return response;
     }
